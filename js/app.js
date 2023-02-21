c10a202872b458f690fc6826a5c0659f461087ae
MandapatisureshQA <114691739+MandapatisureshQA@users.noreply.github.com>
2023-02-21 20:09:38 +0530

    s

--- js/app.js
+++ js/app.js
@@ -14,6 +14,24 @@ closeButton.addEventListener("click", closeSidebar);
 
 
 
+const hamburger1 = document.querySelector(".middle-nav-hamburger");
+const closeButton1 = document.querySelector(".close-button1");
+const sidebarContainer1 = document.querySelector(".hero-sidebar-container");
+
+const openSidebar1 = () => {
+    sidebarContainer1.classList.add("show-sidebar");
+    }
+
+const closeSidebar1 = () => {
+    sidebarContainer1.classList.remove("show-sidebar");
+    }
+
+hamburger1.addEventListener("click", openSidebar1 );
+closeButton1.addEventListener("click", closeSidebar1);
+
+
+
+
 
 
 //nav bar sticky
@@ -68,5 +86,4 @@ window.addEventListener("scroll", function()
         navbarContainer1.classList.remove("sticky-nav");
     }
 });
-
 
