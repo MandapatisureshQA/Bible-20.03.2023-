861d95b1283f219fc20177f122efa9a9009eee40
MandapatisureshQA <114691739+MandapatisureshQA@users.noreply.github.com>
2023-02-23 00:59:37 +0530

    s

--- js/app1.js
+++ js/app1.js
@@ -15,3 +15,17 @@ closeButton1.addEventListener("click", closeSidebar1);
 
 
 
+const navbarContainer1 = document.querySelector(".middle-nav-header");
+window.addEventListener("scroll", function()
+{
+    const scrollNumber = window.scrollY;
+    const targetNumber = window.innerWidth  >= 992 ? 70 : 50 ;
+    if(scrollNumber >= targetNumber)
+    {
+        navbarContainer1.classList.add("sticky-nav");
+    }
+    else
+    {
+        navbarContainer1.classList.remove("sticky-nav");
+    }
+});
